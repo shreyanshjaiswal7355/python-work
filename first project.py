e2ba1814e08695ca621afaa79da09ae4c2290ac4
shreyanshjaiswal7355 <shreyanshjaiswal1469@gmail.com>
2024-09-07 20:06:44 +0530

    Add files via upload

---
+++ first project.py
@@ -0,0 +1,21 @@
+'''#q.1 wap check if a number entered by the user is odd or even
+n=int(input("enter any number\n"))
+if n%2==0:
+    print("even")
+else:
+    print("odd")
+#q.2 wap to find the greatest of 3 numbers entered by the user
+n1,n2,n3=int(input("enter value for n1:")),int(input("enter value for n2:")),int(input("enter value for n3:"))
+if(n1>n2 and n1>n3):
+    print(" a is a greater number")
+elif(n2>n1 and n2>n3):
+    print("b is a greater number")
+else:
+    print("c is a greater number")
+#q.3wap to check if a number is a multiple of 7 or not
+n=int(input("enter any no."))
+x=n%7
+if x==0:
+    print("divisible by 7")
+else:
+    print("not divisible by 7")'''
